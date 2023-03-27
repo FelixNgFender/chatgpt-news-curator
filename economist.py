f52ce0535d54b42eafdd4c5ca84409ae7cab9352
Thinh Nguyen <ngthinh302@gmail.com>
2023-03-27 02:02:16 -0400

    Fix scraping last page for BBC

--- economist.py
+++ economist.py
@@ -9,34 +9,34 @@ import time
 driver = webdriver.Chrome()
 
 # Load the search results page
-url = "https://www.bbc.co.uk/search?q=chatgpt"
+url = "https://www.economist.com/search?q=chatgpt"
 driver.get(url)
 
 links = []
 
 # Click the div until it cannot be clicked anymore
-for i in range(2, 7):
+for i in range(2, 11):
     load_more_button = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//a[@href='/search?q=chatgpt&d=SEARCH_PS&page=" + str(i) + "']"))
+        EC.presence_of_element_located((By.XPATH, "//a[@href='?q=chatgpt&page=" + str(i) + "']"))
     )
     time.sleep(1)  # Wait for new articles to load
     # Extract the article URLs from the search results page
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     # Target the a elements
-    anchors = soup.find_all("a", {"class": "ssrcss-rl2iw9-PromoLink e1f5wbog1"})
+    anchors = soup.find_all("a", {"class": "_search-result"})
     # Append list of links to the end of links
     links.extend([anchor["href"] for anchor in anchors])
+    
     load_more_button.click()
-
     # Account for the last page
-    if i == 6:
+    if i == 10:
         time.sleep(1)  # Wait for new articles to load
         # Extract the article URLs from the search results page
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
         # Target the a elements
-        anchors = soup.find_all("a", {"class": "ssrcss-rl2iw9-PromoLink e1f5wbog1"})
+        anchors = soup.find_all("a", {"class": "_search-result"})
         # Append list of links to the end of links
         links.extend([anchor["href"] for anchor in anchors])
 
